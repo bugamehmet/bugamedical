fbe0d2d746608953783b3a40b29fc47f4fa1afb6
bugamehmet <bugamehmetcan@gmail.com>
2023-10-15 16:36:38 +0300

    rotalar

--- app.js
+++ app.js
@@ -11,9 +11,21 @@ app.use('/assets', express.static('assets'));
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req,res)=>{
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/viewsindex.html");
+})
+app.get('/about', (req,res)=>{
+  res.sendFile(__dirname + "/about.html")
+})
+app.get('/wht-bugamed', (req,res)=>{
+  res.sendFile(__dirname + 'why-bugamed.html')
+})
+app.get('contactus', (req,res)=>{
+  res.sendFile(__dirname + 'contactus.html')
+})
+app.get('parts-catalog', (req, res)=>{
+  res.sendFile(__dirname + 'parts-catalog.html')
 })
 
-app.listen(3001, ()=>{
+app.listen(5001, ()=>{
   console.log('Server http://localhost:5001 adresinde başladı');
 })
