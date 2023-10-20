f92caf272905ae26a91774b746862f9ba9b8d7d4
bugamehmet <bugamehmetcan@gmail.com>
2023-10-20 19:42:32 +0300

    db-bağlantısı

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
-
+const connection = require('./db');
+const { error } = require('winston');
 app.use('/assets', express.static('assets')); 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('views'));
+
+
+connection.query('SELECT model_name FROM models', (err, data)=>{
+  if(err){throw err}
+  console.log(data)
+})
+
+
+
 
 
 app.listen(5001, ()=>{
---
+++ db.js
@@ -0,0 +1,11 @@
+const mysql= require('mysql')
+const config = require('./config')
+
+const connection = mysql.createConnection(config.db)
+
+connection.connect((error)=>{
+  if(error) throw error
+  else console.log('bağlanıldı')
+})
+
+module.exports = connection;
