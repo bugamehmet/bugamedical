2a69192702a43296e896b8bc5808325042ef561f
bugamehmet <bugamehmetcan@gmail.com>
2025-07-19 17:39:50 +0300

    config

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+/*
 const config = {
 	db: {
 		host: 'localhost',
@@ -6,5 +7,14 @@ const config = {
 		database: 'bugaMed',
 	},
 };
+*/
 
-module.exports = config;
+const connection = mysql.createConnection({
+    host     : process.env.DB_HOST,     // Render'dan gelecek
+    user     : process.env.DB_USER,     // Render'dan gelecek
+    password : process.env.DB_PASSWORD, // Render'dan gelecek
+    database : process.env.DB_NAME,     // Render'dan gelecek
+    port     : process.env.DB_PORT      // Render'dan gelecek
+});
+module.exports = connection;
+
