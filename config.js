be51fd166778d43f525a2c05c9da5b15400ab075
Mehmet Can Buga <bugamehmetcan@gmail.com>
2025-07-19 17:40:09 +0300

    Update config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+
+const connection = mysql.createConnection({
+    host     : process.env.DB_HOST,     // Render'dan gelecek
+    user     : process.env.DB_USER,     // Render'dan gelecek
+    password : process.env.DB_PASSWORD, // Render'dan gelecek
+    database : process.env.DB_NAME,     // Render'dan gelecek
+    port     : process.env.DB_PORT      // Render'dan gelecek
+});
+module.exports = connection;
